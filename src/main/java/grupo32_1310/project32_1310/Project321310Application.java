dd0b70baa9a02984e0dc13b42ce30f56fd41c447
Huacal <juanm6263@gmail.com>
2021-10-13 21:36:00 -0500

    Primer version Spring Boot

---
+++ src/main/java/grupo32_1310/project32_1310/Project321310Application.java
@@ -0,0 +1,13 @@
+package grupo32_1310.project32_1310;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project321310Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project321310Application.class, args);
+	}
+
+}
---
+++ src/test/java/grupo32_1310/project32_1310/Project321310ApplicationTests.java
@@ -0,0 +1,13 @@
+package grupo32_1310.project32_1310;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project321310ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
